0e896c5739121085bdeee393ecc0ba805697613a
Dave Boling <kadowki@gmail.com>
2014-12-15 13:35:46 -0600

    Updating for Travis CI and travis.yml

--- labTest.sh
+++ labTest.sh
@@ -1,2 +1,3 @@
 #DATABASE_URL=postgres://soyaku@localhost/evernode_test AWS_BUCKET=db-evernode-test lab -v -m 5000 
-DATABASE_URL=postgres://soyaku@localhost/evernode_test AWS_BUCKET=db-evernode-test make test 
+#DATABASE_URL=postgres://soyaku@localhost/evernode_test AWS_BUCKET=db-evernode-test make test 
+DATABASE_URL=postgres://soyaku@localhost/evernode_test AWS_BUCKET=db-evernode-test npm test
---
+++ test/scripts/create-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#creates the schema and functions for SQL
+psql $1 -f ../../db/tables/users.sql
+psql $1 -f ../../db/tables/notes.sql
+psql $1 -f ../../db/tables/photos.sql
+psql $1 -f ../../db/tables/tags.sql
+psql $1 -f ../../db/tables/notes_tags.sql
+
+psql $1 -f ../../db/functions/add_note.sql
+psql $1 -f ../../db/functions/nuke_note.sql
+psql $1 -f ../../db/functions/show_note.sql
+psql $1 -f ../../db/functions/query_notes.sql
